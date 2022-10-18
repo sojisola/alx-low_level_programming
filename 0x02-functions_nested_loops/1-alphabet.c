3da2f5d6b9d97e2bf6625c5c12694584eab3f94d
sojisola <oluwaseunfunmi4u2c@gmail.com>
2022-10-18 05:57:27 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_alphabet - print alphabets in lower case
+*
+* Return: 0
+*/
+void print_alphabet(void)
+{
+int c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
